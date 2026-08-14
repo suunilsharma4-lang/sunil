0dcda443637c64df8c3269d36eda0ba7d49aa411
suunilsharma4-lang <suunilsharma4@gmail.com>
2026-08-14 12:32:36 +0545

    Add DEFAULT_LOGO constant with Base64 data

---
+++ src/constants/logoData.ts
@@ -0,0 +1,3 @@
+// Sunshine Computer Institute & Service Center Logo Base64
+export const DEFAULT_LOGO = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBD..."; 
+// (नोट: यहाँ तपाईंको लोगोको Base64 Data String राखिन्छ)
