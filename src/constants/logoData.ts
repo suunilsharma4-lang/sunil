07a66fef0a9251303e548c381ad4e21ae67f0932
suunilsharma4-lang <suunilsharma4@gmail.com>
2026-08-14 12:43:40 +0545

    Delete src/constants/logoData.ts

--- src/constants/logoData.ts
+++
@@ -1,3 +0,0 @@
-// Sunshine Computer Institute & Service Center Logo Base64
-export const DEFAULT_LOGO = "https://i.ibb.co/4nNb3z83/Whats-App-Image-2026-08-14-at-11-48-11-AM.jpg"; 
-// (नोट: यहाँ तपाईंको लोगोको Base64 Data String राखिन्छ)
